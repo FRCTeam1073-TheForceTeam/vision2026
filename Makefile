8130d39f0cdf8e4fe59653f0de48465e80d0cc00
Todd Pack <todd.pack@frc1073.org>
2025-12-26 21:15:26 -0500

    Adding basic make of skeleton for local wpilib client install.

--- Makefile
+++ Makefile
@@ -1,24 +1,10 @@
 # Define variables for the compiler and flags
 CXX := g++
-CXXFLAGS := -Wall -Wextra -std=c++17 -Iinclude
-TARGET := main
-SRCS := src/$(wildcard *.cpp)
-OBJS := $(SRCS:.cpp=.o)
+CXXFLAGS := -Wall -Wextra -std=c++20 -Iinclude -I/usr/local/include/ntcore -I/usr/local/include/wpiutil -L/usr/local/lib
+LIBS := -lntcore -lwpiutil
 
-# Default target (builds the executable)
-all: $(TARGET)
-
-# Rule to link object files into the final executable
-$(TARGET): $(OBJS)
-	$(CXX) $(CXXFLAGS) $(OBJS) -o $(TARGET)
-
-# Pattern rule to compile .cpp files into .o files
-%.o: %.cpp
-	$(CXX) $(CXXFLAGS) -c $< -o $@
-
-# Target to run the program
-run: all
-	./$(TARGET)
+skeleton: src/skeleton.cpp
+	$(CXX) $(CXXFLAGS) -o skeleton src/skeleton.cpp $(LIBS)
 
 # Target to clean up generated files
 clean:
---
+++ src/skeleton.cpp
@@ -0,0 +1,23 @@
+#include <chrono>
+#include <thread>
+#include <format>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/DoubleTopic.h>
+
+int main() {
+  auto inst = nt::NetworkTableInstance::GetDefault();
+  auto table = inst.GetTable("datatable");
+  auto xSub = table->GetDoubleTopic("x").Subscribe(0.0);
+  auto ySub = table->GetDoubleTopic("y").Subscribe(0.0);
+  inst.StartClient4("example client");
+  inst.SetServerTeam(1073);  // where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
+  inst.StartDSClient();  // recommended if running on DS computer; this gets the robot IP from the DS
+  while (true) {
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(1s);
+    double x = xSub.Get();
+    double y = ySub.Get();
+    fmt::print("X: {} Y: {}\n", x, y);
+  }
+}
